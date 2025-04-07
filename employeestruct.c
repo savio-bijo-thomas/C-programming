8903431c2c20a36f9c10953d1591a1c3acc8d508
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2025-04-07 09:39:29 +0530

    Add files via upload

---
+++ employeestruct.c
@@ -0,0 +1,30 @@
+/*
+Program to print n employee details using structure
+Author:Savio
+Date:02-04-2025
+*/
+#include<stdio.h>
+typedef struct{
+  char name[20];
+  int id;
+  float salary;
+}student;
+int main(){
+  int limit;
+  printf("Enter the number of the employee:");
+  scanf("%d",&limit);
+  student s[limit];
+  for(int i=0;i<limit;i++){
+    printf("Enter the name of the employee:");
+    getchar();
+    gets(s[i].name);
+    printf("\nEnter the id:");
+    scanf("%d",&s[i].id);
+    printf("\nEnter the salary:");
+    scanf("%f",&s[i].salary);
+  }
+  for(int i=0;i<limit;i++){
+    printf("\nName:%s,Id:%d,Salary:%f",s[i].name,s[i].id,s[i].salary);
+  }
+  return 0;
+}
